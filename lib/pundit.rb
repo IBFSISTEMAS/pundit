0c590f73e781e108bdcd06d0a69620dbdb3b872e
Randrade88 <39090374+Randrade88@users.noreply.github.com>
2020-02-10 15:16:48 -0300

    Add files via upload

--- lib/pundit.rb
+++ lib/pundit.rb
@@ -218,13 +218,11 @@ module Pundit
     query ||= "#{action_name}?"
      
     @_pundit_policy_authorized = true
-    byebug
-  if record.is_a?(Array)
-    
+
+  if record.is_a?(Array)    
     policy = policy_class ? policy_class.new(pundit_user, record[1]) : policy(record[1])
   else
-    policy = policy_class ? policy_class.new(pundit_user, record) : policy(record)
-    
+    policy = policy_class ? policy_class.new(pundit_user, record) : policy(record)    
   end
   
     raise NotAuthorizedError, query: query, record: record, policy: policy unless policy.public_send(query)

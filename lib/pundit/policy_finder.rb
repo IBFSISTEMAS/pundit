1a7bbed21a69ac55b5b219bec8d79e6515430af2
Randrade88 <39090374+Randrade88@users.noreply.github.com>
2020-02-11 10:02:30 -0300

    Add files via upload

--- lib/pundit/policy_finder.rb
+++ lib/pundit/policy_finder.rb
@@ -17,7 +17,7 @@ module Pundit
     def initialize(object)
       byebug
       if object.is_a?(Array)
-        @object = object[1]
+        @object = object[0]
       else
       @object = object
     end
